aba5a47650307af706f73f2c779204bfa7b74b33
superartyom <artyom.s.yakovlev@gmail.com>
2021-09-12 19:54:49 +0400

    code reformated

--- Artyom_Day_4_Homework.py
+++ Artyom_Day_4_Homework.py
@@ -5,23 +5,23 @@
 num = 1
 for i in range(3):
     for j in range(3):
-        print(f'{num*num} ', end='')
+        print(f'{num * num} ', end='')
         num += 1
     print()
 print('█' * 100)
 # 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
 # Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
 num = 1
-list=[]
+list = []
 for i in range(3):
     for j in range(3):
-        list.append(num*num)
+        list.append(num * num)
         num += 1
 print(list, '\n', '█' * 100, sep='')
 # 4. Count the number of 'b's in the given string. DO NOT use the count() method.
 # Հաշվել տրված սթրինգում 'b'-երի քանակը։ Չօգտագործել count() մեթոդը։
 nonsense = 'Blinking blindly, brainy Bob brings beautiful beer bottles beneath blue butterflies billowing brilliantly.'
-total=0
+total = 0
 for char in nonsense:
     if char == 'b' or char == 'B':
         total += 1
@@ -30,6 +30,6 @@ print(total, '\n', '█' * 100, sep='')
 # Գրել ծրագիր, որը կտպի n թվի ֆակտորիալը։
 fnum = int(input('Input number: '))
 fact = 1
-for u in range(1, fnum+1):
+for u in range(1, fnum + 1):
     fact *= u
 print(fact, '\n', '█' * 100, sep='')
