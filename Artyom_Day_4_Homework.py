ade723c048295339e50ace24d29eca021bcbdc54
superartyom <artyom.s.yakovlev@gmail.com>
2021-09-15 21:45:32 +0400

    2

--- Artyom_Day_4_Homework.py
+++ Artyom_Day_4_Homework.py
@@ -17,7 +17,7 @@ print(lst_1)
 print('█' * 100)
 # 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
 # Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
-lst_1 = [[[num**2 for num in range(1, 10)] for j in range (1, 4) ] for i in range(1, 4)]
+lst_1 = [[num**2 for num in range (1, 4) ] for i in range(1, 4)]
 print(lst_1)
 print('█' * 100)
 # 4. Count the number of 'b's in the given string. DO NOT use the count() method.
