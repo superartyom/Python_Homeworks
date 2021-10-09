26004536e5a253216b8e713098f94706ff6e692d
superartyom <artyom.s.yakovlev@gmail.com>
2021-10-09 22:06:53 +0400

    Music Player

---
+++ MusicPlayer.py
@@ -0,0 +1,38 @@
+"""OOP"""
+
+# MusicPlayer homework
+# Now we are going to add some more methods to our classes. It makes sense to have an add_song and remove_song
+# methods in our playlist class, so we can modify our playlist whenever we'd like to.
+# So:
+#   a) Create an add_song method in Playlist class. The method should take a Song as a parameter (or name, artist
+#   etc. e.g. the parameters needed to create a song) and add it to our song list
+#   b) Create a remove_song method in Playlist class. The method should take a Song as a parameter and remove it
+#   from our song list.
+#   c) Now, we need to develop our Player class. Add a play method. The method will print which song is playing now.
+#   By default when we press play on a player it starts playing the first song, so you can do just that. Also,
+#   your player needs to know whether it plays something at the moment or no (so a boolean attribute can be useful).
+#   d) Create a method to show the current songs' information. If no song is playing, let the user know.
+#   e) Create two methods - next_song and prev_song. When we call these methods, the next song in the playlist
+#   will start playing.
+#   f) Create a shuffle method, which will shuffle our playlist.
+# When you've reached this point, feel free to add some other methods, which you'll find useful (for example search
+# for a song, show the playlist, etc.). Good luck!
+
+# Մեր կլասերը ընլայնելու ժամանակն է։ Նախ մեր Playlist կլասին ավելացնենք add_song և remove_song մեթոդներ։
+#   a) Ստեղծել add_song մեթոդ, որը կվերցնի մեկ արգումենտ՝ Song, կամ չորս արգումենտ, որոնցով հնարավոր է ստեղծել Song տիպի
+#   օբյեկտ։
+#   b) Ստեղծել remove_song մեթոդ, որը կվերցնի մեկ արգումենտ` Song, և կջնջի տվյալ երգը song_list-ից։
+#   c) Player կլասին ավելացնել play մեթոդ։ Մեթոդը կտպի, թե որ երգն է միացել։ Նվագարկիչները սովորաբար սկսում են երգել
+#   առաջին երգից։ Կարող եք մեթոդը հենց այդպես իմպլեմենտացնել։
+#   d) Ստեղծել մեթոդ, որը ցույց կտա միացած երգի ինֆորմացիան։ Եթե ոչ մի երգ միացած չէ, տեղեկացնել։
+#   e) Ստեղծել երկու մեթոդ` next_song և prev_song։ Այս մեթոդները կանչելուց համապատասխանաբար կմիանա հաջորդ կամ նախորդ
+#   երգը
+#   f) Ստեղծել shuffle մեթոդ, որը կխառնի playlist-ի երգերը։
+# Այս ամենը ստեղծելուց հետո կարող եք ավելացնել ցանկացած այլ մեթոդ, որ անհրաժեշտ կհամարեք։ Բոլորիդ հաջողությու՛ն։
+class Song:
+    pass
+class PlayList:
+    def __init__(self):
+        self.songs: [Song] = []
+
+    def load_songs(self):
